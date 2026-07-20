d7eeb14af55f506df19977fd79073b9985296373
Odair Cesar Ferreira <odairferreira97@gmail.com>
2026-07-19 21:23:07 -0300

    fix

--- resources/views/components/layout/header.blade.php
+++ resources/views/components/layout/header.blade.php
@@ -1,7 +1,7 @@
 @php
     $navLinks = [
         ['label' => 'Sobre', 'url' => route('about')],
-        ['label' => 'Serviços', 'url' => route('servicos')],
+        ['label' => 'Serviços', 'url' => route('services.index')],
         ['label' => 'Processo', 'url' => route('home').'#processo'],
         ['label' => 'Trabalhos', 'url' => route('home').'#trabalhos'],
         ['label' => 'Blog', 'url' => route('blog.index')],
